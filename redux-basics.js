cd637220590ad3f9f60825aa0d1d8a25d8aa85df
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-11-06 22:22:17 +0100

    redux basics: dispattching actions

--- redux-basics.js
+++ redux-basics.js
@@ -9,6 +9,18 @@ const initialState = {
 // Reducers
 
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
@@ -17,5 +29,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Actions
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
 
 // Subscription
