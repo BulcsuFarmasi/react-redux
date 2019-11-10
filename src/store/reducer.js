050676c345d02a4f4c4240c4dbaf6049367f3d44
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-11-10 20:57:12 +0100

    dispatching more actions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,8 +5,22 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
     return {
-      ...state,
       counter: state.counter + 1
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1
+    };
+  }
+  if (action.type === "ADD") {
+    return {
+      counter: state.counter + 5
+    };
+  }
+  if (action.type === "SUBTRACT") {
+    return {
+      counter: state.counter - 5
     };
   }
   return state;
