2863ab3d86e902f508296d3b014f629a7b3ea1d8
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-11-17 12:54:26 +0100

    using utility function to make reducer leaner

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -11,8 +11,8 @@ const saveResult = result => {
 export const storeResult = result => {
   return (dispatch, getState) => {
     setTimeout(() => {
-      const oldCounter = getState().ctr.counter;
-      console.log(oldCounter);
+      //   const oldCounter = getState().ctr.counter;
+      //   console.log(oldCounter);
       dispatch(saveResult(result));
     }, 2000);
   };
